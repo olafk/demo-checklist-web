40c0763aaf8f328c9b7d8d4274ce9b823ee16048
Olaf Kock <olaf.kock+github@liferay.com>
2021-07-15 09:58:15 +0200

    links now work on instances that don't have /group/guest

--- src/main/java/com/liferay/sales/checklist/impl/PasswordPolicyChecklistProvider.java
+++ src/main/java/com/liferay/sales/checklist/impl/PasswordPolicyChecklistProvider.java
@@ -18,9 +18,9 @@ public class PasswordPolicyChecklistProvider implements ChecklistProvider {
 		try {
 			PasswordPolicy passwordPolicy = passwordPolicyLocalService.getDefaultPasswordPolicy(themeDisplay.getCompanyId());
 			boolean changeRequired = passwordPolicy.getChangeRequired();
-			return new ChecklistItem(!changeRequired, "password-policy-change-required", "/group/guest/~/control_panel/manage?p_p_id=com_liferay_password_policies_admin_web_portlet_PasswordPoliciesAdminPortlet&p_p_lifecycle=0&_com_liferay_password_policies_admin_web_portlet_PasswordPoliciesAdminPortlet_mvcPath=%2Fedit_password_policy.jsp&_com_liferay_password_policies_admin_web_portlet_PasswordPoliciesAdminPortlet_passwordPolicyId=" + passwordPolicy.getPasswordPolicyId());
+			return new ChecklistItem(!changeRequired, "password-policy-change-required", "/group/control_panel/manage?p_p_id=com_liferay_password_policies_admin_web_portlet_PasswordPoliciesAdminPortlet&p_p_lifecycle=0&_com_liferay_password_policies_admin_web_portlet_PasswordPoliciesAdminPortlet_mvcPath=%2Fedit_password_policy.jsp&_com_liferay_password_policies_admin_web_portlet_PasswordPoliciesAdminPortlet_passwordPolicyId=" + passwordPolicy.getPasswordPolicyId());
 		} catch (PortalException e) {
-			return new ChecklistItem(false, "password-policy-change-required", "/group/guest/~/control_panel/manage?p_p_id=com_liferay_password_policies_admin_web_portlet_PasswordPoliciesAdminPortlet", e.getClass() + " " + e.getMessage());
+			return new ChecklistItem(false, "password-policy-change-required", "/group/control_panel/manage?p_p_id=com_liferay_password_policies_admin_web_portlet_PasswordPoliciesAdminPortlet", e.getClass() + " " + e.getMessage());
 		}
 	}
 
