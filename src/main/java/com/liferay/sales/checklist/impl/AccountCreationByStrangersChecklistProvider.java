870130896e78893eb5015e893e18cafa0fe82884
Olaf Kock <olaf.kock+github@liferay.com>
2021-09-02 11:51:30 +0200

    These values are configurable through the UI as well

--- src/main/java/com/liferay/sales/checklist/impl/AccountCreationByStrangersChecklistProvider.java
+++ src/main/java/com/liferay/sales/checklist/impl/AccountCreationByStrangersChecklistProvider.java
@@ -1,9 +1,16 @@
 package com.liferay.sales.checklist.impl;
 
+import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.util.PrefsPropsUtil;
+import com.liferay.portal.util.PropsUtil;
 import com.liferay.portal.util.PropsValues;
 import com.liferay.sales.checklist.api.ChecklistItem;
 import com.liferay.sales.checklist.api.ChecklistProvider;
+
+import javax.portlet.PortletPreferences;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -13,8 +20,27 @@ public class AccountCreationByStrangersChecklistProvider implements ChecklistPro
 
 	@Override
 	public ChecklistItem check(ThemeDisplay themeDisplay) {
-		boolean accountCreationByStrangers = PropsValues.COMPANY_SECURITY_STRANGERS;
-		return new ChecklistItem(!accountCreationByStrangers, "strangers-can-create-accounts", "https://docs.liferay.com/portal/7.3-latest/propertiesdoc/portal.properties.html#Company", "company.security.strangers");
+		PortletPreferences preferences = PrefsPropsUtil.getPreferences(
+				themeDisplay.getCompanyId(), true);
+
+		boolean accountCreationByStrangers = _getPrefsPropsBoolean(
+				preferences, themeDisplay.getCompany(), PropsKeys.COMPANY_SECURITY_STRANGERS,
+				PropsValues.COMPANY_SECURITY_STRANGERS);
+		
+		return new ChecklistItem(!accountCreationByStrangers, "strangers-can-create-accounts", "/group/control_panel/manage?p_p_id=com_liferay_configuration_admin_web_portlet_InstanceSettingsPortlet&_com_liferay_configuration_admin_web_portlet_InstanceSettingsPortlet_mvcRenderCommandName=%2Fconfiguration_admin%2Fview_configuration_screen&_com_liferay_configuration_admin_web_portlet_InstanceSettingsPortlet_configurationScreenKey=general-authentication", "company.security.strangers");
 	}
 
+	private static boolean _getPrefsPropsBoolean(
+		PortletPreferences portletPreferences, Company company, String name,
+		boolean defaultValue) {
+
+		String value = portletPreferences.getValue(
+			name, PropsUtil.get(company, name));
+
+		if (value != null) {
+			return GetterUtil.getBoolean(value);
+		}
+
+		return defaultValue;
+	}
 }
--- src/main/java/com/liferay/sales/checklist/impl/MailLoginChecklistProvider.java
+++ src/main/java/com/liferay/sales/checklist/impl/MailLoginChecklistProvider.java
@@ -1,9 +1,16 @@
 package com.liferay.sales.checklist.impl;
 
+import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.util.PrefsPropsUtil;
+import com.liferay.portal.util.PropsUtil;
 import com.liferay.portal.util.PropsValues;
 import com.liferay.sales.checklist.api.ChecklistItem;
 import com.liferay.sales.checklist.api.ChecklistProvider;
+
+import javax.portlet.PortletPreferences;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -12,10 +19,45 @@ public class MailLoginChecklistProvider implements ChecklistProvider {
 
 	@Override
 	public ChecklistItem check(ThemeDisplay themeDisplay) {
-		boolean prepopulate = PropsValues.COMPANY_LOGIN_PREPOPULATE_DOMAIN;
-		String authType = PropsValues.COMPANY_SECURITY_AUTH_TYPE;
+		PortletPreferences preferences = PrefsPropsUtil.getPreferences(
+				themeDisplay.getCompanyId(), true);
+
+		boolean prepopulate = _getPrefsPropsBoolean(
+				preferences, themeDisplay.getCompany(), PropsKeys.COMPANY_LOGIN_PREPOPULATE_DOMAIN,
+				PropsValues.COMPANY_LOGIN_PREPOPULATE_DOMAIN);;
+		String authType = _getPrefsPropsString(
+				preferences, themeDisplay.getCompany(), PropsKeys.COMPANY_SECURITY_AUTH_TYPE,
+				PropsValues.COMPANY_SECURITY_AUTH_TYPE);
 		prepopulate &= "emailAddress".equals(authType);
-		return new ChecklistItem(!prepopulate, "login-prepopulate", "https://docs.liferay.com/portal/7.3-latest/propertiesdoc/portal.properties.html#Company", "company.login.prepopulate.domain");
+		
+		return new ChecklistItem(!prepopulate, "login-prepopulate", "/group/control_panel/manage?p_p_id=com_liferay_configuration_admin_web_portlet_InstanceSettingsPortlet&_com_liferay_configuration_admin_web_portlet_InstanceSettingsPortlet_mvcRenderCommandName=%2Fconfiguration_admin%2Fview_configuration_screen&_com_liferay_configuration_admin_web_portlet_InstanceSettingsPortlet_configurationScreenKey=general-authentication", "company.login.prepopulate.domain");
+	}
+	
+	private static boolean _getPrefsPropsBoolean(
+		PortletPreferences portletPreferences, Company company, String name,
+		boolean defaultValue) {
+
+		String value = portletPreferences.getValue(
+			name, PropsUtil.get(company, name));
+
+		if (value != null) {
+			return GetterUtil.getBoolean(value);
+		}
+
+		return defaultValue;
 	}
 
+	private static String _getPrefsPropsString(
+		PortletPreferences portletPreferences, Company company, String name,
+		String defaultValue) {
+
+		String value = portletPreferences.getValue(
+			name, PropsUtil.get(company, name));
+
+		if (value != null) {
+			return value;
+		}
+
+		return defaultValue;
+	}
 }
