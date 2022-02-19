6f5213e19a35c7db35f567b04744bbc66cc801bf
Olaf Kock <olaf.kock+github@liferay.com>
2022-02-19 11:43:26 +0100

    bump version

--- src/main/java/com/liferay/sales/checklist/impl/ChecklistConfiguration.java
+++ src/main/java/com/liferay/sales/checklist/impl/ChecklistConfiguration.java
@@ -15,7 +15,7 @@ public interface ChecklistConfiguration {
 	 * @return
 	 */
 	@Meta.AD(
-            deflt = "1.0.8",
+            deflt = "1.1.0",
             description = "config-updated-version-check-description",
             name = "config-updated-version-check-name",
             required = false
